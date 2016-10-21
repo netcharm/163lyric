937af7012b5cd5922de92ce7f2d45af7de94dbe6
netcharm <netcharm@163.com>
2016-10-22 01:54:55 +0800

    add cover image preview window on search result window

--- 163lyric/163lyric/SearchResult.Designer.cs
+++ 163lyric/163lyric/SearchResult.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormSearchResult));
             this.lvResult = new System.Windows.Forms.ListView();
             this.No = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
@@ -42,6 +43,18 @@
             this.btnOk = new System.Windows.Forms.Button();
             this.queryWorker = new System.ComponentModel.BackgroundWorker();
             this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.contextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tsmiCopyPhotoURL = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiCopyCoverURL = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsmiCopyArtist = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiCopyAlbum = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiCopyTitle = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiCopyPublisher = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsmiDisplayPhoto = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiDisplayCover = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // lvResult
@@ -56,6 +69,7 @@
             this.musicCompany,
             this.musicCover,
             this.musicPhotos});
+            this.lvResult.ContextMenuStrip = this.contextMenu;
             this.lvResult.FullRowSelect = true;
             this.lvResult.GridLines = true;
             this.lvResult.MultiSelect = false;
@@ -64,6 +78,7 @@
             this.lvResult.UseCompatibleStateImageBehavior = false;
             this.lvResult.View = System.Windows.Forms.View.Details;
             this.lvResult.VirtualMode = true;
+            this.lvResult.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.lvResult_ItemSelectionChanged);
             this.lvResult.RetrieveVirtualItem += new System.Windows.Forms.RetrieveVirtualItemEventHandler(this.lvResult_RetrieveVirtualItem);
             this.lvResult.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lvResult_MouseDoubleClick);
             // 
@@ -126,6 +141,82 @@
             resources.ApplyResources(this.progressBar, "progressBar");
             this.progressBar.Name = "progressBar";
             // 
+            // contextMenu
+            // 
+            this.contextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiCopyPhotoURL,
+            this.toolStripMenuItem1,
+            this.tsmiCopyCoverURL,
+            this.tsmiCopyArtist,
+            this.tsmiCopyAlbum,
+            this.tsmiCopyTitle,
+            this.tsmiCopyPublisher,
+            this.toolStripMenuItem2,
+            this.tsmiDisplayPhoto,
+            this.tsmiDisplayCover});
+            this.contextMenu.Name = "contextMenu";
+            this.contextMenu.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            resources.ApplyResources(this.contextMenu, "contextMenu");
+            this.contextMenu.Opened += new System.EventHandler(this.contextMenu_Opened);
+            // 
+            // tsmiCopyPhotoURL
+            // 
+            this.tsmiCopyPhotoURL.Name = "tsmiCopyPhotoURL";
+            resources.ApplyResources(this.tsmiCopyPhotoURL, "tsmiCopyPhotoURL");
+            this.tsmiCopyPhotoURL.Click += new System.EventHandler(this.tsmiCopyPhotoURL_Click);
+            // 
+            // tsmiCopyCoverURL
+            // 
+            this.tsmiCopyCoverURL.Name = "tsmiCopyCoverURL";
+            resources.ApplyResources(this.tsmiCopyCoverURL, "tsmiCopyCoverURL");
+            this.tsmiCopyCoverURL.Click += new System.EventHandler(this.tsmiCopyCoverURL_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            resources.ApplyResources(this.toolStripMenuItem1, "toolStripMenuItem1");
+            // 
+            // tsmiCopyArtist
+            // 
+            this.tsmiCopyArtist.Name = "tsmiCopyArtist";
+            resources.ApplyResources(this.tsmiCopyArtist, "tsmiCopyArtist");
+            this.tsmiCopyArtist.Click += new System.EventHandler(this.tsmiCopyArtist_Click);
+            // 
+            // tsmiCopyAlbum
+            // 
+            this.tsmiCopyAlbum.Name = "tsmiCopyAlbum";
+            resources.ApplyResources(this.tsmiCopyAlbum, "tsmiCopyAlbum");
+            this.tsmiCopyAlbum.Click += new System.EventHandler(this.tsmiCopyAlbum_Click);
+            // 
+            // tsmiCopyTitle
+            // 
+            this.tsmiCopyTitle.Name = "tsmiCopyTitle";
+            resources.ApplyResources(this.tsmiCopyTitle, "tsmiCopyTitle");
+            this.tsmiCopyTitle.Click += new System.EventHandler(this.tsmiCopyTitle_Click);
+            // 
+            // tsmiCopyPublisher
+            // 
+            this.tsmiCopyPublisher.Name = "tsmiCopyPublisher";
+            resources.ApplyResources(this.tsmiCopyPublisher, "tsmiCopyPublisher");
+            this.tsmiCopyPublisher.Click += new System.EventHandler(this.tsmiCopyPublisher_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            resources.ApplyResources(this.toolStripMenuItem2, "toolStripMenuItem2");
+            // 
+            // tsmiDisplayPhoto
+            // 
+            this.tsmiDisplayPhoto.Name = "tsmiDisplayPhoto";
+            resources.ApplyResources(this.tsmiDisplayPhoto, "tsmiDisplayPhoto");
+            this.tsmiDisplayPhoto.Click += new System.EventHandler(this.tsmiDisplayPhoto_Click);
+            // 
+            // tsmiDisplayCover
+            // 
+            this.tsmiDisplayCover.Name = "tsmiDisplayCover";
+            resources.ApplyResources(this.tsmiDisplayCover, "tsmiDisplayCover");
+            this.tsmiDisplayCover.Click += new System.EventHandler(this.tsmiDisplayCover_Click);
+            // 
             // FormSearchResult
             // 
             this.AcceptButton = this.btnOk;
@@ -137,10 +228,12 @@
             this.Controls.Add(this.lvResult);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.HelpButton = true;
             this.Name = "FormSearchResult";
             this.ShowInTaskbar = false;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormSearchResult_FormClosing);
             this.Load += new System.EventHandler(this.FormSearchResult_Load);
+            this.contextMenu.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -160,5 +253,16 @@
         private System.Windows.Forms.ColumnHeader No;
         private System.ComponentModel.BackgroundWorker queryWorker;
         private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.ContextMenuStrip contextMenu;
+        private System.Windows.Forms.ToolStripMenuItem tsmiCopyPhotoURL;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem tsmiCopyCoverURL;
+        private System.Windows.Forms.ToolStripMenuItem tsmiCopyArtist;
+        private System.Windows.Forms.ToolStripMenuItem tsmiCopyAlbum;
+        private System.Windows.Forms.ToolStripMenuItem tsmiCopyTitle;
+        private System.Windows.Forms.ToolStripMenuItem tsmiCopyPublisher;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem tsmiDisplayPhoto;
+        private System.Windows.Forms.ToolStripMenuItem tsmiDisplayCover;
     }
 }
--- 163lyric/163lyric/SearchResult.cs
+++ 163lyric/163lyric/SearchResult.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Windows.Forms;
 using _163music;
@@ -77,6 +79,31 @@ namespace _163lyric
             lblResultState.Text = $"Total {nMusic.ResultTotal} results. Current display {mItems.Count} results.";
         }
 
+        public void ShowImageBox( Image image )
+        {
+            Button btnCancel = new Button();
+            //btnCancel.Visible = false;
+            btnCancel.SendToBack();
+            btnCancel.DialogResult = DialogResult.Cancel;
+
+            PictureBox imgBox = new PictureBox();
+            imgBox.Dock = DockStyle.Fill;
+            imgBox.SizeMode = PictureBoxSizeMode.Zoom;
+            imgBox.Image = image;
+
+            Form fm = new Form();
+            //fm.Size = new Size( 256, 256 );
+            fm.ClientSize = new Size( 256, 256 );
+            fm.StartPosition = FormStartPosition.CenterParent;
+            fm.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+
+            fm.Controls.Add( imgBox );
+            fm.Controls.Add( btnCancel );
+            fm.CancelButton = btnCancel;
+
+            fm.ShowDialog( this );
+        }
+
         public FormSearchResult()
         {
             InitializeComponent();
@@ -104,13 +131,12 @@ namespace _163lyric
             //queryMusics( mId );
         }
 
-        private void lvResult_RetrieveVirtualItem( object sender, RetrieveVirtualItemEventArgs e )
+        private void FormSearchResult_FormClosing( object sender, FormClosingEventArgs e )
         {
-            //check to see if the requested item is currently in the cache
-            if ( e.ItemIndex >= 0 && e.ItemIndex < mItems.Count && e.ItemIndex < Items.Count && Items.Count >0 )
+            if ( queryWorker.WorkerSupportsCancellation == true )
             {
-                e.Item = Items[e.ItemIndex];
-                e.Item.BackColor = ( e.ItemIndex % 2 == 1 ) ? Color.AliceBlue : e.Item.BackColor;
+                // Cancel the asynchronous operation.
+                queryWorker.CancelAsync();
             }
         }
 
@@ -124,12 +150,40 @@ namespace _163lyric
             }
         }
 
+        private void lvResult_RetrieveVirtualItem( object sender, RetrieveVirtualItemEventArgs e )
+        {
+            //check to see if the requested item is currently in the cache
+            if ( e.ItemIndex >= 0 && e.ItemIndex < mItems.Count && e.ItemIndex < Items.Count && Items.Count >0 )
+            {
+                try
+                {
+                    if ( Items is List<ListViewItem> && Items[e.ItemIndex] is ListViewItem )
+                    {
+                        if( Items[e.ItemIndex].SubItems.Count == 8 )
+                        {
+                            e.Item = Items[e.ItemIndex];
+                            e.Item.BackColor = ( e.ItemIndex % 2 == 1 ) ? Color.AliceBlue : e.Item.BackColor;
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+
+                }
+            }
+        }
+
         private void lvResult_MouseDoubleClick( object sender, MouseEventArgs e )
         {
             if ( lvResult.SelectedIndices.Count > 0 )
             {
                 btnOk.PerformClick();
             }
+        }
+
+        private void lvResult_ItemSelectionChanged( object sender, ListViewItemSelectionChangedEventArgs e )
+        {
+            contextMenu.Tag = e.Item;
         }
 
         private void queryWorker_DoWork( object sender, DoWorkEventArgs e )
@@ -213,12 +267,118 @@ namespace _163lyric
             lblResultState.Text = $"Total {nMusic.ResultTotal} results. Current display {mItems.Count} results.";
         }
 
-        private void FormSearchResult_FormClosing( object sender, FormClosingEventArgs e )
+        private void tsmiCopyPhotoURL_Click( object sender, EventArgs e )
         {
-            if ( queryWorker.WorkerSupportsCancellation == true )
+            if ( contextMenu.Tag is ListViewItem )
             {
-                // Cancel the asynchronous operation.
-                queryWorker.CancelAsync();
+                var text = ((ListViewItem)contextMenu.Tag).SubItems[7].Text;
+                Clipboard.SetText( text );
+            }
+        }
+
+        private void tsmiCopyCoverURL_Click( object sender, EventArgs e )
+        {
+            if ( contextMenu.Tag is ListViewItem )
+            {
+                var text = ((ListViewItem)contextMenu.Tag).SubItems[6].Text;
+                Clipboard.SetText( text );
+            }
+        }
+
+        private void tsmiCopyArtist_Click( object sender, EventArgs e )
+        {
+            if ( contextMenu.Tag is ListViewItem )
+            {
+                var text = ((ListViewItem)contextMenu.Tag).SubItems[3].Text;
+                Clipboard.SetText( text );
+            }
+        }
+
+        private void tsmiCopyAlbum_Click( object sender, EventArgs e )
+        {
+            if ( contextMenu.Tag is ListViewItem )
+            {
+                var text = ((ListViewItem)contextMenu.Tag).SubItems[4].Text;
+                Clipboard.SetText( text );
+            }
+        }
+
+        private void tsmiCopyTitle_Click( object sender, EventArgs e )
+        {
+            if ( contextMenu.Tag is ListViewItem )
+            {
+                var text = ((ListViewItem)contextMenu.Tag).SubItems[2].Text;
+                Clipboard.SetText( text );
+            }
+        }
+
+        private void tsmiCopyPublisher_Click( object sender, EventArgs e )
+        {
+            if ( contextMenu.Tag is ListViewItem )
+            {
+                var text = ((ListViewItem)contextMenu.Tag).SubItems[5].Text;
+                Clipboard.SetText( text );
+            }
+        }
+
+        private void contextMenu_Opened( object sender, EventArgs e )
+        {
+            //if ( contextMenu.Tag is ListViewItem )
+            //{
+            //    using ( WebClient client = new WebClient() )
+            //    {
+            //        string url_photo = ((ListViewItem)contextMenu.Tag).SubItems[7].Text;
+            //        //byte [] data = client.DownloadDataAsync( url );
+            //        byte [] data_photo = client.DownloadData( url_photo );
+            //        using ( MemoryStream mem = new MemoryStream( data_photo ) )
+            //        {
+            //            tsmiCopyPhotoURL.Image = Image.FromStream( mem );
+            //        }
+
+            //        string url_cover = ((ListViewItem)contextMenu.Tag).SubItems[6].Text;
+            //        byte [] data_cover = client.DownloadData( url_cover );
+            //        using ( MemoryStream mem = new MemoryStream( data_cover ) )
+            //        {
+            //            tsmiCopyCoverURL.Image = Image.FromStream( mem );
+            //        }
+
+            //        //client.DownloadFileAsync( new Uri( url ), @"c:\temp\image35.png" );
+            //        //client.DownloadFile( new Uri( url ), @"c:\temp\image35.png" );
+            //        //tsmiCopyCoverURL.Image =
+            //    }
+            //}
+        }
+
+        private void tsmiDisplayPhoto_Click( object sender, EventArgs e )
+        {
+            if ( contextMenu.Tag is ListViewItem )
+            {
+                using ( WebClient client = new WebClient() )
+                {
+                    string url_photo = ((ListViewItem)contextMenu.Tag).SubItems[7].Text;
+                    byte [] data_photo = client.DownloadData( url_photo );
+                    using ( MemoryStream mem = new MemoryStream( data_photo ) )
+                    {
+                        ShowImageBox( Image.FromStream( mem ) );
+                    }
+                    
+                }
+            }
+        }
+
+        private void tsmiDisplayCover_Click( object sender, EventArgs e )
+        {
+            if ( contextMenu.Tag is ListViewItem )
+            {
+                using ( WebClient client = new WebClient() )
+                {
+                    string url_cover = ((ListViewItem)contextMenu.Tag).SubItems[6].Text;
+                    byte [] data_cover = client.DownloadData( url_cover );
+                    using ( MemoryStream mem = new MemoryStream( data_cover ) )
+                    {
+                        ShowImageBox( Image.FromStream( mem ) );
+                    }
+                }
             }
         }
     }
